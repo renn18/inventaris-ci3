8b5d96379066a4919cbde9d34cfb08e83fc2380a
Renn <ppskdos@gmail.com>
2024-09-06 01:33:22 +0800

    first commit

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -49,7 +49,7 @@ if (!$this->session->has_userdata('login_session')) {
     <div id="wrapper">
 
         <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-secondary sidebar sidebar-dark accordion" id="accordionSidebar">
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url() ?>home">
